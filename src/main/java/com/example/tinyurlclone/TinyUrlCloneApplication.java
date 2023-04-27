19ff87b832ead7c8b428a4ce4f24aff8e4ab004d
dinhlockt02 <dinhlockt02@gmail.com>
2023-04-27 15:09:51 +0700

    Init project

---
+++ src/main/java/com/example/tinyurlclone/TinyUrlCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinyurlclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyUrlCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tinyurlclone/TinyUrlCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinyurlclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyUrlCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
